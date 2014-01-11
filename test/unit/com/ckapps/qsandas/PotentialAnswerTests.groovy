0226473f63440c0c16138baa6b58f80e9863253e
cchanley2003 <cchanley2003@yahoo.com>
2014-01-11 10:43:20 -0700

    New test classes.

---
+++ test/unit/com/ckapps/qsandas/PotentialAnswerTests.groovy
@@ -0,0 +1,17 @@
+package com.ckapps.qsandas
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(PotentialAnswer)
+class PotentialAnswerTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
---
+++ test/unit/com/ckapps/qsandas/QuestionTests.groovy
@@ -0,0 +1,17 @@
+package com.ckapps.qsandas
+
+
+
+import grails.test.mixin.*
+import org.junit.*
+
+/**
+ * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
+ */
+@TestFor(Question)
+class QuestionTests {
+
+    void testSomething() {
+       fail "Implement me"
+    }
+}
